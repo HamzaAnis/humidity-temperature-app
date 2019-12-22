c5b90c5a13d104ae677836a0452649354486fc02
Hamza Anis <hamzaanis9514@gmail.com>
2019-12-22 04:02:01 -0800

    Formatting

--- reference/SerialExample/Excel Sample/Program.cs
+++ reference/SerialExample/Excel Sample/Program.cs
@@ -64,10 +64,51 @@ namespace Excel_Sample
             }
             return table;
         }
+        private static DataTable GetTable1()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Patient", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            table.Rows.Add(25, "Indocin", "David", new DateTime(2000, 1, 1));
+            table.Rows.Add(50, "Enebrel", "Sam", new DateTime(2000, 1, 2));
+            table.Rows.Add(10, "Hydralazine", "Christoff", new DateTime(2000, 1, 3));
+            table.Rows.Add(21, "Combivent", "Janet", new DateTime(2000, 1, 4));
+            table.Rows.Add(100, "Dilantin", "Melanie", new DateTime(2000, 1, 5));
+            return table;
+        }
+        public static void Create1(String filePath)
+        {
+            var workbook = new XLWorkbook();
+            var ws = workbook.Worksheets.Add("Row Settings");
+
+            var row1 = ws.Row(2);
+            row1.Style.Fill.BackgroundColor = XLColor.Red;
+            row1.Height = 30;
+
+            var row2 = ws.Row(4);
+            row2.Style.Fill.BackgroundColor = XLColor.DarkOrange;
+            row2.Height = 3;
+
+            workbook.SaveAs(filePath);
+        }
+        private class Person
+        {
+            [XLColumn(Header = "House Street")]
+            public String House { get; set; }
+
+            public String Name { get; set; }
+            public Int32 Age { get; set; }
+
+            [XLColumn(Header = "Class Type")]
+            public static String ClassType { get { return nameof(Person); } }
+        }
 
         static void Main(string[] args)
-        {
-            Create("Check.xlsx");
+        { 
+            Create1("Check2.xlsx");
         }
     }
 
